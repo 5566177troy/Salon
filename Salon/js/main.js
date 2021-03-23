b9df568e6966fe65ad9058d434c205d171dd986b
Troy04152 <77441819+5566177troy@users.noreply.github.com>
2021-03-23 03:13:09 +0300

    Salon

---
+++ Salon/js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('.header__slider-items').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    
+  });
+  $('.services-box__items').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+
+  });
+
+});
